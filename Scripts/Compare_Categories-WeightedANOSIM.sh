6b738300849bd968d8cbb56da0cff37c04c5dae8
zcbtm97 <37797297+zcbtm97@users.noreply.github.com>
2018-05-08 18:07:03 +0100

    Update Compare_Categories-WeightedANOSIM.sh

--- Scripts/Compare_Categories-WeightedANOSIM.sh
+++ Scripts/Compare_Categories-WeightedANOSIM.sh
@@ -17,15 +17,7 @@ export TMPDIR=~/qiime_tmp
 
 #comparingcategoriesforweighteddistances
 
-compare_categories.py --method anosim -i updatedpcoa/weighted_unifrac_dm.txt -m map.tsv -c SamplePhosphorus -o statisticaltests/weightedanosim/anosim_P -n 999
-
-compare_categories.py --method anosim -i updatedpcoa/weighted_unifrac_dm.txt -m map.tsv -c SampleNitrogen -o statisticaltests/weightedanosim/anosim_N -n 999
-
-compare_categories.py --method anosim -i updatedpcoa/weighted_unifrac_dm.txt -m map.tsv -c SamplePotassium -o statisticaltests/weightedanosim/anosim_K -n 999
-
 compare_categories.py --method anosim -i updatedpcoa/weighted_unifrac_dm.txt -m map.tsv -c SamplePh -o statisticaltests/weightedanosim/anosim_pH -n 999
-
-compare_categories.py --method anosim -i updatedpcoa/weighted_unifrac_dm.txt -m map.tsv -c SampleDepth -o statisticaltests/weightedanosim/anosim_D -n 999
 
 #deactivate qiime
 source deactivate
